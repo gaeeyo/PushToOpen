ca884be964b3cd2361b81276cf8600e3d916ea5d
gaeeyo <iknnofm0126@gmail.com>
2020-10-20 20:52:06 +0900

    鍵の操作結果がわかりにくかったので処理中と結果の通知を分離、絵文字を使って成功か失敗かすぐわかるように

--- app/src/main/java/jp/syoboi/android/pushtoopen/task/SetLockTask.java
+++ app/src/main/java/jp/syoboi/android/pushtoopen/task/SetLockTask.java
@@ -18,6 +18,14 @@ public class SetLockTask extends SesameTask<ActionResult> {
 
     @Override
     protected ActionResult exec(@NonNull Sesame sesame) throws Exception {
+//        if (BuildConfig.DEBUG) {
+//            Thread.sleep(5000);
+//            JSONObject j = new JSONObject();
+//            j.put("status", ActionResult.STATUS_TERMINATED);
+//            j.put("successful", true);
+//            return new ActionResult(j);
+//        }
+
         String taskId = sesame.setLock(mDeviceId, mLock);
 
         Thread.sleep(1000);
